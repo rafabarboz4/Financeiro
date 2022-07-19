82b546f1c49c03c44cbf00a2b0381cc61add7057
Rafa <rvbarboza44@gmail.com>
2022-07-19 18:29:33 -0300

    Adicionando

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const transactionsUl = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
 
 const dummyTransactions = [
     { id: 1, name: 'Bolo de brigadeiro', amount: -20},
     { id: 2, name: 'Salario', amount: 500},
-    { id: 3, name: 'Torta', amount: -5}
+    { id: 3, name: 'Torta', amount: -5},
 ]
 
 const addTransactionsintoDOM = transaction => {
@@ -20,4 +23,27 @@ const addTransactionsintoDOM = transaction => {
     transactionsUl.append(li)
 }
 
-addTransactionsintoDOM(dummyTransactions[2])
+const updateBalanceValues = () =>{
+    const transactionAmounts = dummyTransactions
+            .map(transaction => transaction.amount)
+    const total = transactionAmounts
+            .reduce((accumulator, transaction) => accumulator + transaction, 0)
+            .toFixed(2)
+    const income = transactionAmounts
+            .filter(value => value > 0)
+            .reduce((accumulator, value) => accumulator + value, 0)
+            .toFixed(2)
+    const expense = Math.abs(transactionAmounts
+            .filter(value => value < 0)
+            .reduce((accumulator, value) => accumulator + value, 0))
+            .toFixed(2)
+            incomeDisplay.textContent = `R$ ${income}`
+            expenseDisplay.textContent = `R$ ${expense}`
+            balanceDisplay.textContent = `R$ ${total}`
+}
+
+const init = () =>{ 
+    dummyTransactions.forEach(addTransactionsintoDOM)
+    updateBalanceValues()
+}
+init()
