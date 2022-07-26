fca93bd1214dbe51973a2f9b5363972ef17173bf
Rafa <rvbarboza44@gmail.com>
2022-07-26 13:42:31 -0300

    Finalizado

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ const transactionsUl = document.querySelector('#transactions')
 const incomeDisplay = document.querySelector('#money-plus')
 const expenseDisplay = document.querySelector('#money-minus')
 const balanceDisplay = document.querySelector('#balance')
+const form = document.querySelector('#form')
+const inputTransactionName = document.querySelector('#text')
+const inputTransactionAmount = document.querySelector('#amount')
 
-const dummyTransactions = [
-    { id: 1, name: 'Bolo de brigadeiro', amount: -20},
-    { id: 2, name: 'Salario', amount: 500},
-    { id: 3, name: 'Torta', amount: -5},
-]
+const localstorageTransactions = JSON.parse(localStorage.getItem('transactions'))
+let transactions = localStorage = localStorage.getItem('transactions') !== null ? localstorageTransactions : []
+
+const removeTransaction = ID =>{
+    transactions = transactions.filter(transaction => transaction.id !== ID)
+    updateLocalStorage()
+    init()
+}
 
 const addTransactionsintoDOM = transaction => {
     const operator = transaction.amount < 0 ? '-' : '+'
@@ -18,13 +24,14 @@ const addTransactionsintoDOM = transaction => {
     
     li.classList.add(CSSclass)
     li.innerHTML = `
-        ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span> <button class="delete-btn">x</button>
+        ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}
+        </span> <button class="delete-btn" onClick= "removeTransaction(${transaction.id})">x</button>
     `
     transactionsUl.append(li)
 }
 
 const updateBalanceValues = () =>{
-    const transactionAmounts = dummyTransactions
+    const transactionAmounts = transactions
             .map(transaction => transaction.amount)
     const total = transactionAmounts
             .reduce((accumulator, transaction) => accumulator + transaction, 0)
@@ -42,8 +49,36 @@ const updateBalanceValues = () =>{
             balanceDisplay.textContent = `R$ ${total}`
 }
 
-const init = () =>{ 
-    dummyTransactions.forEach(addTransactionsintoDOM)
+const init = () =>{
+    transactionsUl.innerHTML = ''
+    transactions.forEach(addTransactionsintoDOM)
     updateBalanceValues()
 }
 init()
+
+updateLocalStorage = () =>{
+    localStorage.setItem('transactions', JSON.stringify(transactions))
+}
+
+const generateID = () => Math.round(Math.random() * 10000)
+
+form.addEventListener('submit', event =>{
+    event.preventDefault()
+    const transactionName = inputTransactionName.value.trim()
+    const transactionAmount = inputTransactionAmount.value.trim() 
+
+    if(transactionName == '' || transactionAmount == ''){
+        alert('[Erro] um dos campos estão vazios.')
+        return
+    }
+    const transaction = { id: generateID(),
+                          name: transactionName,
+                          amount: Number(transactionAmount)}
+
+    transactions.push(transaction)
+    init()
+    updateLocalStorage()
+
+    inputTransactionName.value = ''
+    inputTransactionAmount.value = ''
+})
